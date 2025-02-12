2fc60ac9c1cb99c3979949fe6cea9cbf35e471a7
Luca Mendez <lucamendez018@gmail.com>
2025-02-12 19:51:24 -0300

    add: versioning

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="flask_shoppingcart",
-    version="0.1.0",
+    version="0.2.0",
     author="Brixt18",
     author_email="",
     description="Add a shopping cart to your Flask app",
